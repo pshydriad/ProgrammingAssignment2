3c2259b1dc1954f2e6e3dded5652c66c29f3f662
pshydriad <pshydriad@gmail.com>
2015-02-21 15:06:15 -0500

    Submit for grading
    Used empty shell from class assignment to create completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions.  One of the functions creates a matrix 
+## which includes its inverse. The second one checks to see if the inverse
+## already exists.  If it does, it uses the cached answer.  If not, then it calculates it.
 
-## Write a short comment describing this function
+
+##-------------------------------------------------------
+## Per our class instructions... 
+## This function creates a special "matrix" object that can cache its inverse.
+##-------------------------------------------------------
 
 makeCacheMatrix <- function(x = matrix()) {
 
-}
+        m <- NULL
+        set <- function(y) {
+            x <<- y
+            m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+    }
+    
 
-
-## Write a short comment describing this function
+##-------------------------------------------------------
+## Per our class instructions... 
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve should retrieve the inverse from the cache.
+##-------------------------------------------------------
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$mgetinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
